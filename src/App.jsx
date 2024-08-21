872151b1a6bb78c69d784fa20f2f76fa5c9a3466
gellyzoid <39223824+gellyzoid@users.noreply.github.com>
2024-08-21 20:59:33 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Main from "./components/Main";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import WeatherInfo from "./components/WeatherInfo";
 import WeatherDisplay from "./components/WeatherDisplay";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route element={<Main />}>
             <Route index element={<WeatherDisplay />} replace to="/" />
             <Route path="info" element={<WeatherInfo />} />
+            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </BrowserRouter>
