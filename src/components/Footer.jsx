1a83c7410c37f683c5f4741b5ffc1a3c727d2013
gellyzoid <39223824+gellyzoid@users.noreply.github.com>
2024-08-21 21:20:16 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ function Footer() {
     <footer className="bg-gray-800 text-white py-4">
       <div className="container mx-auto text-center">
         <p className="text-xs">
-          &copy; {new Date().getFullYear()} Angel Victorio
+          &copy; {new Date().getFullYear()} AVCWS
         </p>
         <div className="mt-1">
           <a
-            href="https://www.angelvictorio.com"
+            href=""
             target="_NEW"
             className="text-gray-400 hover:text-white mx-2 text-xs"
           >
